3bb7953ca7b6ca68d3a1ad64e7086f0163dfbb06
Dmitrii Kalashnikov <dmitri.112.k@gmail.com>
2025-03-04 14:17:24 +0100

    header and footer

---
+++ particials/header.php
@@ -0,0 +1,30 @@
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Tiya Golf Club - Free HTML CSS Template</title>
+
+    <!-- CSS FILES -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+
+    <link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,wght@0,400;0,500;0,700;1,400&display=swap" rel="stylesheet">
+
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <link href="css/bootstrap-icons.css" rel="stylesheet">
+
+    <link href="css/templatemo-tiya-golf-club.css" rel="stylesheet">
+
+    <!--
+
+    TemplateMo 587 Tiya Golf Club
+
+    https://templatemo.com/tm-587-tiya-golf-club
+
+    -->
+</head>
